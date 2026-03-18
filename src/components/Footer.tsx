ee1e198b69fbfab2564093930849524ee1eb1deb
Barry Chen <barry.phasel235@gmail.com>
2026-03-18 11:30:21 -0700

    added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+    return (
+        <footer className="w-full bg-white p-5 text-xl">
+            <p className="text-black text-center">
+            Hobson Lodge <br /><br />
+            224 Hobson Street, Auckland CBD, Auckland 1010, New Zealand <br /><br />
+            Phone <br />
+            09 972 9019 <br /><br />
+            Email <br />
+            info@hobsonlodge.co.nz <br /><br />
+            Reception hours: Monday - Sunday   9.30am - 5pm <br />
+            Check in time:               2pm - 5pm <br />
+            Afterhours: Please contact the hotel directly
+            </p>
+        </footer>
+    )
+}
