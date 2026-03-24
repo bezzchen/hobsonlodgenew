4de17f25d8fc6d98664f12c198b14269c8ecd857
Barry Chen <barry.phasel235@gmail.com>
2026-03-23 21:56:00 -0400

    fixed main content styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,10 @@ export default function Home() {
         <h1 className="text-8xl text-black mt-8">
           HOBSON LODGE
         </h1>
-        <h2>
+        <h2 className="text-6xl text-black">
           IN THE HEART OF CBD
         </h2>
-        <p>
+        <p className="max-w-4xl text-black text-xl mx-auto">
           Hobson Lodge is newly refurbished with decorated bed rooms, hotel grade mattress, spacious living room and kitchen. Hobson Lodge is good to stay here for a night or for weeks, stay here while on business trip or at some kind of concerts or work and study in Auckland - either way our lodge is the best possible variant. Feel free to contact us anytime in case you have any questions or concerns.
         </p>
       </div>
