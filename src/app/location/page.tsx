cbc994480dc6dcd026597906283c865bfbfc016b
Barry Chen <barry.phasel235@gmail.com>
2026-03-29 22:57:29 -0400

    added location hero

--- src/app/location/page.tsx
+++ src/app/location/page.tsx
@@ -1,7 +1,14 @@
+import Image from 'next/image';
 export default function location() {
     return (
         <main>
-            
+            <Image
+                src="/images/0.jpg"
+                alt="hero image"
+                width={1600}
+                height={800}
+                className="w-full object-cover h-[690px] pt-16"
+            />
         </main>
     )
 }
