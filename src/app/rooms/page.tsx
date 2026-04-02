0196a66bbe33b2d197c3b86dd205801679f131b3
Barry Chen <barry.phasel235@gmail.com>
2026-04-02 19:31:35 -0400

    added rooms hero

--- src/app/rooms/page.tsx
+++ src/app/rooms/page.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+
 export default function rooms() {
     return (
-        <main>
-            
+        <main className="bg-white">
+            <Image
+                src="/images/0.jpg"
+                alt="hero image"
+                width={1600}
+                height={800}
+                className="w-full object-cover h-[680px]"
+            />
+            <div className="text-center flex flex-col gap-y-8">
+                <h1 className="text-6xl text-black pt-10">
+                    OUR ROOMS
+                </h1>   
+            </div>
         </main>
     )
 }
