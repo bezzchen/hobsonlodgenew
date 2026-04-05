7663c0de5d6080621ff9def03fee3a0760e3c739
Barry Chen <barry.phasel235@gmail.com>
2026-04-05 17:37:10 -0400

    added rules title

--- src/app/rules/page.tsx
+++ src/app/rules/page.tsx
@@ -9,6 +9,11 @@ export default function rules() {
                 height={800}
                 className="w-full object-cover h-[680px] pt-10"
             />
+            <div className="flex flex-col gap-y-8 text-center text-black">
+                <h1 className="text-6xl pt-6"> 
+                    RULES
+                </h1>
+            </div>
         </main>
     )
 }
