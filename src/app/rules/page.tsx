411916e6bc2df151789b4be655d756d55aa9219b
Barry Chen <barry.phasel235@gmail.com>
2026-04-05 17:34:30 -0400

    added rules hero

--- src/app/rules/page.tsx
+++ src/app/rules/page.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
 export default function rules() {
     return (
-        <main>
-            
+        <main className="bg-white">
+            <Image
+                src="/images/0.jpg"
+                alt="herorules"
+                width={1600}
+                height={800}
+                className="w-full object-cover h-[680px] pt-10"
+            />
         </main>
     )
 }
