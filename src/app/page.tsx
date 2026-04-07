b7255f89f180e22635eb199404781a3a12e98f70
Barry Chen <barry.phasel235@gmail.com>
2026-04-07 17:51:32 -0400

    formatted main text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,17 +19,17 @@ export default function Home() {
           IN THE HEART OF CBD
         </h2>
         <p className="max-w-4xl text-black text-xl mx-auto pb-10">
-        Hobson Lodge offers a comfortable and welcoming stay in the centre of Auckland, ideal for solo travellers, backpackers, and long-stay guests.
-Conveniently located just 300 metres from Sky Tower, with easy access to public transport and the motorway, getting around the city is simple and efficient. The entrance is right by the main route connecting to State Highway 1, making travel to and from the lodge quick and convenient.
-Designed with a focus on air quality, comfort, and personal space, we provide a more enjoyable and relaxing hostel experience:
-Privacy curtains for every bed, creating your own personal space 
-Individual power outlet at each bed for convenient charging 
-Large windows in every room with plenty of natural light 
-Fresh air ventilation system in all rooms, providing a cleaner and more comfortable environment 
-Secure double-door access system (guest-only entry) for added safety 
-Guests can also enjoy a clean and well-maintained environment, a fully equipped kitchen, and a relaxed, friendly atmosphere.
-More privacy, more comfort, more fresh air – a better hostel experience.
-Whether you're staying for a night or a few weeks, Hobson Lodge is a great choice for business trips, concerts, work, or study in Auckland.
+        Hobson Lodge offers a comfortable and welcoming stay in the centre of Auckland, ideal for solo travellers, backpackers, and long-stay guests. <br />
+Conveniently located just 300 metres from Sky Tower, with easy access to public transport and the motorway, getting around the city is simple and efficient. The entrance is right by the main route connecting to State Highway 1, making travel to and from the lodge quick and convenient. <br />
+Designed with a focus on air quality, comfort, and personal space, we provide a more enjoyable and relaxing hostel experience: <br />
+Privacy curtains for every bed, creating your own personal space <br />
+Individual power outlet at each bed for convenient charging <br />
+Large windows in every room with plenty of natural light <br />
+Fresh air ventilation system in all rooms, providing a cleaner and more comfortable environment <br />
+Secure double-door access system (guest-only entry) for added safety <br />
+Guests can also enjoy a clean and well-maintained environment, a fully equipped kitchen, and a relaxed, friendly atmosphere.<br />
+More privacy, more comfort, more fresh air – a better hostel experience.<br />
+Whether you're staying for a night or a few weeks, Hobson Lodge is a great choice for business trips, concerts, work, or study in Auckland.<br />
 If you have any questions, feel free to contact us anytime – we’re always happy to help.
         </p>
       </div>
