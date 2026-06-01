edb5600ebccd2c4f4ae60f1d02908fb64049af99
Barry Chen <barry.phasel235@gmail.com>
2026-05-31 22:36:03 -0700

    added gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "./globals.css";
@@ -33,6 +34,7 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-XN4MLT8KYQ" />
     </html>
   );
 }
